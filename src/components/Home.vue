fb6b0cf8b78217c785a3ab3fb2a867baca0f8e6e
ChenZhijie667 <1031061181@qq.com>
2021-04-14 17:13:20 +0800

    完成登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+     <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        logout(){
+            //清空token
+            window.sessionStorage.removeItem('token');
+            //跳转到登录页
+            this.$router.push("/login");
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+// const routes = [
+  
+// ]
 
 const router = new VueRouter({
-  routes
+  routes:[
+    //如果访问login地址，通过component属性来指定要显示的组件
+    { path: '/login', component: Login},
+    { path: '/', redirect:'/login'},
+    { path: '/home', component: Home},
+  ]
+})
+//挂载路由导航守卫
+router.beforeEach((to,from,next) => {
+  //to表示将要访问的路径
+  //from表示从哪个路径跳转而来
+  //next是一个函数，表示放行
+  //  next() 放行  next('/login')强制跳转到对应页面
+  if(to.path == '/login') return next();
+  //获取token，调用方法
+  const tokenStr = window.sessionStorage.getItem('token');
+  if(!tokenStr)  return next('/login')
+  else return next();
 })
 
 export default router
